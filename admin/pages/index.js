27455f48032f748f400d795a1accc06943ce4dcb
YOUR NAME <YOUR@EMAIL.com>
2024-03-15 17:20:08 +0300

    Are You Ready To Get Higher Salez

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,3 +1,5 @@
+import { FcGoogle } from "react-icons/fc";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
@@ -5,15 +7,15 @@ export default function Home() {
         <h2 className="items-center font-bold">Welcome To Your Dash Mr/Mrs Admin...</h2>
         <label className="relative flex flex-col mt-10">
           <span className="font-medium mb-2">Email</span>
-          <input type="text" value="" placeholder="Email Needed..." className="border-grey-100 focus:border-green-500 outline-none relative bg-transparent text-black focus:ring-0"/>
+          <input type="text" value="" placeholder="Email Needed..." className="border-grey-100 focus:border-green-500 outline-none relative bg-transparent text-black focus:ring-0" />
         </label><label className="relative flex flex-col">
           <span className="font-medium mb-2">Password</span>
-          <input type="text" value="" placeholder="8 characters" className="border-grey-100 focus:border-green-500 outline-none relative bg-transparent text-black focus:ring-0"/>
+          <input type="text" value="" placeholder="8 characters" className="border-grey-100 focus:border-green-500 outline-none relative bg-transparent text-black focus:ring-0" />
         </label>
         <h2 className="items-center text-center font-bold">OR</h2>
         <h4 className="items-center text-center font-light">Login with:</h4>
-        <button className="bg-gray-300 hover:bg-black hover:text-white">
-            Google
+        <button className="bg-gray-300 hover:bg-black hover:text-white flex items-center gap-2 justify-center">
+          <FcGoogle size={20} /> Google
         </button>
         {/* <span>
           <button className="bg-gray-300 hover:bg-black hover:text-white mt-5">
