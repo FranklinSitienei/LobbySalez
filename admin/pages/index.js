6f2d21d5028ca9691236403714cd0d7eba776d49
YOUR NAME <YOUR@EMAIL.com>
2024-03-21 13:02:41 +0300

    Are You Ready To Make Salez

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -3,13 +3,19 @@ import { FcGoogle } from 'react-icons/fc';
 import { FiSun } from 'react-icons/fi';
 import { MdDarkMode } from 'react-icons/md';
 import { useSession, signIn, signOut } from 'next-auth/react';
-import BackgroundImage from '../public/assets/image.jpg'
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { data: session, status } = useSession();
+
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -23,23 +29,18 @@ export default function Home() {
     setPassword(e.target.value);
   };
 
-  const handleSignIn = async (e) => {
-    e.preventDefault();
-    await signIn('credentials', { email, password });
-  };
-
   return (
     <div className={`${darkMode ? 'bg-black' : 'bg-white'} w-screen h-screen flex justify-center items-center`}>
       {/* Conditional rendering for image placement */}
       {!darkMode && (
         <img
-          src={BackgroundImage}
+          src="/assets/image.jpg"
           alt="Image"
           className="max-w-xs h-auto"
           style={{ marginRight: '1rem' }}
         />
       )}
-      <form className={`flex flex-col rounded-lg shadow-md ${darkMode ? 'bg-blue-500' : 'bg-gray-100'} p-4 justify-center`} onSubmit={handleSignIn}>
+      <form className={`flex flex-col rounded-lg shadow-md ${darkMode ? 'bg-blue-500' : 'bg-gray-100'} p-4 justify-center`}>
         {/* Your form content */}
         <label className="relative flex flex-col">
           <span className={`${darkMode ? 'text-white' : 'text-black'} font-medium mb-2`}>Email</span>
@@ -51,14 +52,14 @@ export default function Home() {
         </label>
         <h2 className={`${darkMode ? 'text-white' : 'text-black'} font-bold mt-5 mb-2 items-center text-center`}>OR</h2>
         <h4 className={`${darkMode ? 'text-white' : 'text-black'} font-light mb-2 items-center text-center`}>Login with:</h4>
-        <button type="submit" className={`hover:bg-black hover:text-white flex items-center gap-2 justify-center ${darkMode ? 'text-white' : 'text-black'} bg-gray-300`}>
+        <button onClick={() => signIn('google')} className={`hover:bg-black hover:text-white flex items-center gap-2 justify-center ${darkMode ? 'text-white' : 'text-black'} bg-gray-300`}>
           <FcGoogle size={20} /> Google
         </button>
       </form>
       {/* Conditional rendering for image placement */}
       {darkMode && (
         <img
-          src="/assets/"
+          src="/assets/image2.jpg"
           alt="Image"
           className="max-w-xs h-auto"
           style={{ marginLeft: '1rem' }}
